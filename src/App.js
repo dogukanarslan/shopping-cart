1ddfcdafdfa2e457b9f0f04a27fb292c2b530988
Dogukan Arslan <dogukan2arslan@hotmail.com>
2021-02-12 21:36:25 +0300

    Add home

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
 
-      <div className="content"></div>
+      <div className="content">
+        <Home />
+      </div>
     </div>
   );
 }
