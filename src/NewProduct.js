f96bfb5440bcce17a90cdd99c5611f56ca17d58f
Dogukan Arslan <dogukan2arslan@hotmail.com>
2021-02-16 19:56:16 +0300

    Customize new product page

--- src/NewProduct.js
+++ src/NewProduct.js
@@ -1,7 +1,20 @@
 const NewProduct = () => {
   return (
-    <div>New Product</div>
-   );
-}
+    <div className="product-new">
+      <h2>New Product</h2>
+      <form>
+        <label>Name</label>
+        <input type="text" />
+        <label>Price</label>
+        <input type="number" />
+        <label>Quantity</label>
+        <input type="number" />
+        <label>Description</label>
+        <textarea></textarea>
+        <button>Create</button>
+      </form>
+    </div>
+  );
+};
 
 export default NewProduct;
