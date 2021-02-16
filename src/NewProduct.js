3d1a8ff6ef0fe9c37ffde500e47994c9e6020410
Dogukan Arslan <dogukan2arslan@hotmail.com>
2021-02-16 20:19:47 +0300

    Post data to create new product

--- src/NewProduct.js
+++ src/NewProduct.js
@@ -1,17 +1,56 @@
+import { useState } from 'react';
+import { Redirect } from 'react-router-dom';
+
 const NewProduct = () => {
+  const [name, setName] = useState('');
+  const [price, setPrice] = useState(0);
+  const [quantity, setQuantity] = useState(0);
+  const [description, setDescription] = useState('');
+  const [isPosting, setIsPosting] = useState(false);
+  const [isRedirected, setIsRedirected] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setIsPosting(true);
+    fetch('http://localhost:8000/products', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ name, price, quantity, description })
+    }).then(() => {
+      setIsRedirected(true);
+    });
+  };
+
+  if (isRedirected) {
+    return <Redirect to="/" />;
+  }
+
   return (
     <div className="product-new">
       <h2>New Product</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>Name</label>
-        <input type="text" />
+        <input
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          type="text"
+        />
         <label>Price</label>
-        <input type="number" />
+        <input
+          value={price}
+          onChange={(e) => setPrice(e.target.value)}
+          type="number"
+        />
         <label>Quantity</label>
-        <input type="number" />
+        <input
+          value={quantity}
+          onChange={(e) => setQuantity(e.target.value)}
+          type="number"
+        />
         <label>Description</label>
-        <textarea></textarea>
-        <button>Create</button>
+        <textarea onChange={(e) => setDescription(e.target.value)}></textarea>
+        {!isPosting && <button>Create</button>}
+        {isPosting && <button disabled={isPosting}>Creating</button>}
       </form>
     </div>
   );
