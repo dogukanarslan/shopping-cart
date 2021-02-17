1a64a6c3b6c423f438a3e6a894ddf8bfd6ba013c
Dogukan Arslan <dogukan2arslan@hotmail.com>
2021-02-17 21:19:08 +0300

    Add delete feature

--- src/ProductDetail.js
+++ src/ProductDetail.js
@@ -1,17 +1,33 @@
-import { useParams } from 'react-router-dom';
+import { useParams, Redirect } from 'react-router-dom';
 import useFetch from './useFetch';
+import { useState } from 'react';
 
 const ProductDetail = () => {
   const { id } = useParams();
   const { data: product, isLoading, error, setData } = useFetch(
     `http://localhost:8000/products/${id}`
   );
+  const [isRedirected, setIsRedirected] = useState(false);
+
+  const handleDelete = (e) => {
+    fetch(`http://localhost:8000/products/${id}`, {
+      method: 'DELETE'
+    }).then(() => {
+      setIsRedirected(true);
+    });
+  };
+
+  if (isRedirected) {
+    return <Redirect to="/" />;
+  }
+
   return (
     <div>
       <h4>{product.name}</h4>
       <p>{product.price}</p>
       <p>{product.quantity}</p>
       <p>{product.description}</p>
+      <button onClick={handleDelete}>Delete</button>
     </div>
   );
 };
