d4f9ae5165f867214563252e40d94f8a7c45c5cd
Dogukan Arslan <dogukan2arslan@hotmail.com>
2021-02-21 19:19:39 +0300

    Add bootstrap

--- src/ProductList.js
+++ src/ProductList.js
@@ -2,19 +2,28 @@ import { Link } from 'react-router-dom';
 
 const ProductList = ({ products, addToCart }) => {
   return (
-    <div className="product-list">
-      {products.map((product) => {
-        return (
-          <li className="product-preview" key={product.id}>
-            <Link to={`/products/${product.id}`}>
-              <p>
-                {product.name} ({product.quantity})
-              </p>
-            </Link>
-            <button onClick={() => addToCart(product.id)}>Add</button>
-          </li>
-        );
-      })}
+    <div className="container">
+      <ul className="list-group w-60 m-auto">
+        {products.map((product) => {
+          return (
+            <li className="list-group-item" key={product.id}>
+              <div className="d-flex justify-content-between align-items-center">
+                <Link to={`/products/${product.id}`} className="text-decoration-none text-dark fs-2">
+                  <p className="">{product.name}</p>
+                </Link>
+                <span className="badge bg-primary">{product.quantity}</span>
+              </div>
+
+              <button
+                className="btn btn-sm btn-secondary"
+                onClick={() => addToCart(product.id)}
+              >
+                Add
+              </button>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 };
