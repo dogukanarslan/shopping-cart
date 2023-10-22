4e89cc61d2d7ccf041194172028aa86fec561817
Dogukan Arslan <dogukan2arslan@hotmail.com>
2023-10-22 16:57:27 +0300

    Migrate to Vite

---
+++ src/Navbar.jsx
@@ -0,0 +1,28 @@
+import { Flex, HStack, Text } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+import { Heading } from "@chakra-ui/react";
+
+const Navbar = () => {
+  return (
+    <>
+      <Flex
+        as="nav"
+        bgColor="cyan.700"
+        textColor="white"
+        justifyContent="space-between"
+        
+        padding="1rem"
+        marginBottom="1rem"
+      >
+        <Heading fontSize="1rem">The Shopping Cart</Heading>
+        <HStack spacing="2rem">
+          <Link to="/">Home</Link>
+          <Link to="/cart">Cart</Link>
+          <Link to="/products/create">Create Product</Link>
+        </HStack>
+      </Flex>
+    </>
+  );
+};
+
+export default Navbar;
