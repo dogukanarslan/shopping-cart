1741e3c958092b199232db3ac7b8ddba01520b3e
Dogukan Arslan <dogukan2arslan@hotmail.com>
2023-11-12 21:09:24 +0300

    Delete unnecessary element

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -4,24 +4,21 @@ import { Heading } from "@chakra-ui/react";
 
 const Navbar = () => {
   return (
-    <>
-      <Flex
-        as="nav"
-        bgColor="cyan.700"
-        textColor="white"
-        justifyContent="space-between"
-        
-        padding="1rem"
-        marginBottom="1rem"
-      >
-        <Heading fontSize="1rem">The Shopping Cart</Heading>
-        <HStack spacing="2rem">
-          <Link to="/">Home</Link>
-          <Link to="/cart">Cart</Link>
-          <Link to="/products/create">Create Product</Link>
-        </HStack>
-      </Flex>
-    </>
+    <Flex
+      as="nav"
+      bgColor="cyan.700"
+      textColor="white"
+      justifyContent="space-between"
+      padding="1rem"
+      marginBottom="1rem"
+    >
+      <Heading fontSize="1rem">The Shopping Cart</Heading>
+      <HStack spacing="2rem">
+        <Link to="/">Home</Link>
+        <Link to="/cart">Cart</Link>
+        <Link to="/products/create">Create Product</Link>
+      </HStack>
+    </Flex>
   );
 };
 
