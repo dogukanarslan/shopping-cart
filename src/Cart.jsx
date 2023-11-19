96e68373ffa5551e20f461e1266fef02586588c3
Dogukan Arslan <dogukan2arslan@hotmail.com>
2023-11-20 01:42:27 +0300

    List cart items as table

--- src/Cart.jsx
+++ src/Cart.jsx
@@ -1,25 +1,53 @@
-import { ListItem, Text, UnorderedList } from "@chakra-ui/react";
+import {
+  Heading,
+  TableContainer,
+  Table,
+  Thead,
+  Tr,
+  Th,
+  Tbody,
+  Td,
+} from "@chakra-ui/react";
 
 const Cart = ({ cartItems }) => {
   console.log(cartItems);
+
+  if (cartItems.length === 0) {
+    return <Heading size="md">Cart is empty</Heading>;
+  }
+
   return (
     <>
-      <UnorderedList>
-        {cartItems.map((item) => (
-          <ListItem key={item.id}>
-            {item.name} - {item.quantity}
-          </ListItem>
-        ))}
-      </UnorderedList>
-      <Text>
-        Total: {" "}
-        {cartItems.reduce(
-          (total, currentValue) =>{
-            console.log(currentValue)
-            return total + currentValue.quantity * currentValue.price},
-          0
-        )}
-      </Text>
+      <TableContainer>
+        <Table variant="simpe">
+          <Thead>
+            <Tr>
+              <Th>Name</Th>
+              <Th>Quantity</Th>
+              <Th>Price</Th>
+              <Th>Total</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            {cartItems.map((item) => (
+              <Tr>
+                <Td>{item.name}</Td>
+                <Td>{item.quantity}</Td>
+                <Td>{item.price}</Td>
+                <Td>{item.quantity * item.price}</Td>
+              </Tr>
+            ))}
+          </Tbody>
+        </Table>
+      </TableContainer>
+
+      <Heading size="md">
+        Total Price:{" "}
+        {cartItems.reduce((total, currentValue) => {
+          console.log(currentValue);
+          return total + currentValue.quantity * currentValue.price;
+        }, 0)}
+      </Heading>
     </>
   );
 };
