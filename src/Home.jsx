06715280dc5c169ecae0ac7ca9be12b82b7a3f5b
Dogukan Arslan <dogukan2arslan@hotmail.com>
2024-04-25 01:16:15 +0300

    Delete cart page

--- src/Home.jsx
+++ src/Home.jsx
@@ -7,41 +7,7 @@ const Home = ({ cartItems, setCartItems }) => {
     data: products,
     isLoading,
     error,
-    setData,
   } = useFetch('http://localhost:8000/products');
-
-  const addToCart = (product) => {
-    if (product.quantity === 0) {
-      alert('Out of stock!');
-      return;
-    }
-
-    const newProducts = products.map((item) => {
-      if (item.id === product.id) {
-        return { ...item, quantity: item.quantity - 1 };
-      }
-
-      return item;
-    });
-    const newCartItems = cartItems.some((item) => item.name === product.name)
-      ? cartItems.map((item) =>
-          item.name === product.name
-            ? { ...item, quantity: item.quantity + 1 }
-            : item
-        )
-      : [
-          ...cartItems,
-          {
-            id: Math.random(),
-            name: product.name,
-            quantity: 1,
-            price: product.price,
-          },
-        ];
-
-    setCartItems(newCartItems);
-    setData(newProducts);
-  };
 
   if (isLoading) {
     return <Spinner />;
@@ -51,13 +17,7 @@ const Home = ({ cartItems, setCartItems }) => {
     return error;
   }
 
-  return (
-    <ProductList
-      products={products}
-      cartItems={cartItems}
-      addToCart={addToCart}
-    />
-  );
+  return <ProductList products={products} cartItems={cartItems} />;
 };
 
 export default Home;
