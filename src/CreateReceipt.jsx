719379c5a10cf6ca92a7cc57e4963a7af6090243
Dogukan Arslan <dogukan2arslan@hotmail.com>
2024-05-02 00:29:17 +0300

    Align receipt inputs

--- src/CreateReceipt.jsx
+++ src/CreateReceipt.jsx
@@ -6,14 +6,22 @@ import {
   NumberInput,
   NumberInputField,
   VStack,
-  StackDivider,
+  Text,
   Stack,
+  Card,
+  CardHeader,
+  CardBody,
+  Heading,
 } from '@chakra-ui/react';
 import useFetch from './useFetch';
 
 const CreateReceipt = () => {
   const [name, setName] = useState('');
   const [items, setItems] = useState([]);
+
+  const [itemName, setItemName] = useState('');
+  const [itemPrice, setItemPrice] = useState('');
+  const [itemQuantity, setItemQuantity] = useState('');
 
   const { data: products, isLoading } = useFetch(
     'http://localhost:8000/products'
@@ -37,7 +45,19 @@ const CreateReceipt = () => {
   };
 
   const addItem = () => {
-    setItems((prev) => [...prev, { id: Math.random(), name: '' }]);
+    setItems((prev) => [
+      ...prev,
+      {
+        id: Math.random(),
+        name: itemName,
+        price: itemPrice,
+        quantity: itemQuantity,
+      },
+    ]);
+
+    setItemName('');
+    setItemPrice('');
+    setItemQuantity('');
   };
 
   const addSelectedProduct = (product) => {
@@ -71,7 +91,6 @@ const CreateReceipt = () => {
       />
 
       <Box display="flex" justifyContent="space-between">
-        <Button onClick={addItem}>Add Item</Button>
         <Button type="submit">Create Receipt</Button>
       </Box>
       <Box my={5}>
@@ -92,36 +111,43 @@ const CreateReceipt = () => {
         )}
       </Box>
 
+      <Box mb={2}>
+        <Input
+          value={itemName}
+          mb="10px"
+          onChange={(e) => setItemName(e.target.value)}
+        />
+        <NumberInput
+          mb="10px"
+          value={itemPrice}
+          onChange={(num) => setItemPrice(num)}
+        >
+          <NumberInputField />
+        </NumberInput>
+        <NumberInput
+          mb="10px"
+          value={itemQuantity}
+          onChange={(num) => setItemQuantity(num)}
+        >
+          <NumberInputField />
+        </NumberInput>
+
+        <Button onClick={addItem}>Add Item</Button>
+      </Box>
+
       {items.length === 0 ? (
         'No item added'
       ) : (
-        <VStack spacing={10} divider={<StackDivider />}>
+        <VStack spacing={2}>
           {items.map((item) => (
-            <Box key={item.id} w="100%">
-              <Input
-                mb="10px"
-                placeholder="Item name"
-                value={item.name}
-                onChange={(e) =>
-                  changeItem(item.id, e.target.value, item.price)
-                }
-              />
-              <NumberInput
-                mb="10px"
-                value={item.price}
-                onChange={(e) => changeItem(item.id, item.name, e.target.value)}
-              >
-                <NumberInputField placeholder="Item price" />
-              </NumberInput>
-              <NumberInput
-                value={item.quantity}
-                onChange={(e) =>
-                  changeItem(item.id, item.name, item.price, e.target.value)
-                }
-              >
-                <NumberInputField placeholder="Item quantity" />
-              </NumberInput>
-            </Box>
+            <Card key={item.id} w="100%" variant="outline" size="sm">
+              <CardBody>
+                <Heading size="md">{item.name}</Heading>
+                <Text>Price: {item.price}</Text>
+                <Text>Quantity: {item.quantity}</Text>
+                <Text>Total: {item.quantity * item.price}</Text>
+              </CardBody>
+            </Card>
           ))}
         </VStack>
       )}
