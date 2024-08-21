15f55d41d987ebbb2879b286aad893a67fc0a208
Dogukan Arslan <dogukan2arslan@hotmail.com>
2024-08-21 22:48:44 +0300

    Align layout

--- src/Receipts.jsx
+++ src/Receipts.jsx
@@ -1,6 +1,5 @@
 import { useContext } from 'react';
-import { Link } from 'react-router-dom';
-import { SimpleGrid, Box } from '@chakra-ui/react';
+import { SimpleGrid } from '@chakra-ui/react';
 
 import ReceiptList from './ReceiptList';
 import { ReceiptsContext } from './context/ReceiptsContext';
@@ -11,16 +10,6 @@ export const Receipts = () => {
   return (
     <SimpleGrid columns={4} spacing={5}>
       <ReceiptList receipts={receipts} />
-      <Box
-        border="1px"
-        borderColor="gray.100"
-        borderRadius="md"
-        display="flex"
-        justifyContent="center"
-        alignItems="center"
-      >
-        <Link to="/receipts/create">New Receipt</Link>
-      </Box>
     </SimpleGrid>
   );
 };
