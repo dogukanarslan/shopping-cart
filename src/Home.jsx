221a0d2cf4f857680e568e0cb917668ba187e4f0
Dogukan Arslan <dogukan2arslan@hotmail.com>
2024-10-14 23:49:25 +0300

    Add home card

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,7 +1,23 @@
-import { Redirect } from 'react-router-dom/';
+import { useContext } from 'react';
+import { FiFileText } from 'react-icons/fi';
+import { ReceiptsContext } from './contexts/ReceiptsContext';
+import { Card, CardBody } from '@nextui-org/react';
 
 const Home = () => {
-  return <Redirect to="/receipts" />;
+  const { receipts } = useContext(ReceiptsContext);
+  return (
+    <div>
+      <Card classNames={{ body: 'flex-row items-center' }}>
+        <CardBody>
+          <FiFileText size={32} />
+          <div className="mx-5">
+            <h4 className="text-2xl font-semibold">{receipts.length}</h4>
+            <div className="text-gray-500">Total Receipts</div>
+          </div>
+        </CardBody>
+      </Card>
+    </div>
+  );
 };
 
 export default Home;
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,8 +1,9 @@
 import { Listbox, ListboxItem } from '@nextui-org/react';
-import { FiFile, FiPlusSquare, FiDisc } from 'react-icons/fi';
+import { FiFile, FiPlusSquare, FiDisc, FiHome } from 'react-icons/fi';
 
 const LinkItems = [
-  { name: 'CreateReceipt', icon: <FiPlusSquare />, url: '/receipts/create' },
+  { name: 'Home', icon: <FiHome />, url: '/' },
+  { name: 'Create Receipt', icon: <FiPlusSquare />, url: '/receipts/create' },
   { name: 'Receipts', icon: <FiFile />, url: '/receipts' },
   { name: 'Products', icon: <FiDisc />, url: '/products' },
 ];
@@ -11,9 +12,9 @@ const Sidebar = () => {
   return (
     <div className="fixed h-full w-56 border-r-1 p-6">
       <h1 className="font-bold text-2xl mb-4">Receipts</h1>
-      <Listbox>
+      <Listbox variant="faded">
         {LinkItems.map((link) => (
-          <ListboxItem key={link.name} href={link.url}>
+          <ListboxItem key={link.name} href={link.url} textValue="hi">
             <div className="flex items-center">
               <div className="mr-2">{link.icon}</div> {link.name}
             </div>
