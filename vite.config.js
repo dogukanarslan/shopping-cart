50164e34ea093e634db3a52b0b5d1401d3c2f659
Dogukan Arslan <dogukan2arslan@hotmail.com>
2024-10-17 19:48:42 +0300

    Change output directory to build

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   // depending on your application, base can also be "/"
   base: '',
+  build: {
+    outDir: "build"
+  },
   plugins: [react(), eslint()],
   server: {
     // this ensures that the browser opens upon server start
