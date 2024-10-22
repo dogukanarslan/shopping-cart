ad6ecff8002b920aa04fce5c56b80a2c1e7d68c5
Dogukan Arslan <dogukan2arslan@hotmail.com>
2024-10-23 01:37:17 +0300

    Show products on dashboard

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,18 +1,30 @@
 import { useContext } from 'react';
-import { FiFileText } from 'react-icons/fi';
+import { FiFile, FiTv } from 'react-icons/fi';
 import { ReceiptsContext } from './contexts/ReceiptsContext';
 import { Card, CardBody } from '@nextui-org/react';
+import { ProductsContext } from './contexts/ProductsContext';
 
 const Home = () => {
   const { receipts } = useContext(ReceiptsContext);
+  const { products } = useContext(ProductsContext);
+
   return (
-    <div>
+    <div className="space-y-2">
       <Card classNames={{ body: 'flex-row items-center' }}>
         <CardBody>
-          <FiFileText size={32} />
+          <FiFile size={32} />
           <div className="mx-5">
             <h4 className="text-2xl font-semibold">{receipts.length}</h4>
             <div className="text-gray-500">Total Receipts</div>
+          </div>
+        </CardBody>
+      </Card>
+      <Card classNames={{ body: 'flex-row items-center' }}>
+        <CardBody>
+          <FiTv size={32} />
+          <div className="mx-5">
+            <h4 className="text-2xl font-semibold">{products.length}</h4>
+            <div className="text-gray-500">Total Products</div>
           </div>
         </CardBody>
       </Card>
