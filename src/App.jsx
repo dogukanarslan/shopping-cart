0aa975b20bd81f3265a096ccbd2bcd2f0e4ff8d9
Dogukan Arslan <dogukan2arslan@hotmail.com>
2024-10-31 23:54:42 +0300

    Add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Link } from 'react-router-dom';
 import Home from './Home';
 import { Receipts } from './Receipts';
 import { ReceiptDetail } from './ReceiptDetail';
@@ -6,12 +6,50 @@ import CreateReceipt from './CreateReceipt';
 import { Products } from './Products';
 import Sidebar from './components/Sidebar';
 import CreateProduct from './CreateProduct';
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarMenu,
+  NavbarMenuItem,
+  NavbarMenuToggle,
+} from '@nextui-org/react';
+import { FiFile, FiPlusSquare, FiDisc, FiHome, FiTv } from 'react-icons/fi';
+import { useState } from 'react';
+
+const linkItems = [
+  { name: 'Home', icon: <FiHome />, url: '/' },
+  { name: 'Create Receipt', icon: <FiPlusSquare />, url: '/receipts/create' },
+  { name: 'Receipts', icon: <FiFile />, url: '/receipts' },
+  { name: 'Create Product', icon: <FiDisc />, url: '/products/create' },
+  { name: 'Products', icon: <FiTv />, url: '/products' },
+];
 
 function App() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   return (
     <div className="h-full">
-      <Sidebar />
-      <div className="ml-56 h-full p-4">
+      <Navbar
+        onMenuOpenChange={setIsMenuOpen}
+        disableAnimation
+        maxWidth="full"
+        isMenuOpen={isMenuOpen}
+      >
+        <NavbarContent>
+          <NavbarMenuToggle />
+          <NavbarBrand>Receipts</NavbarBrand>
+        </NavbarContent>
+        <NavbarMenu>
+          {linkItems.map((item) => (
+            <NavbarMenuItem key={item.name}>
+              <Link to={item.url} onClick={() => setIsMenuOpen(false)}>
+                {item.name}
+              </Link>
+            </NavbarMenuItem>
+          ))}
+        </NavbarMenu>
+      </Navbar>
+      <div className="h-full p-4">
         <Switch>
           <Route exact path="/">
             <Home />
