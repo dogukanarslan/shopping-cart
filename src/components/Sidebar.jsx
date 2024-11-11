cdcece7292328c023f7598cfa0fafc3db1695c5a
Dogukan Arslan <dogukan2arslan@hotmail.com>
2024-11-12 00:15:38 +0300

    Hide navbar on large screens

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -5,8 +5,10 @@ import {
   FiHome,
   FiLogOut,
   FiPlusSquare,
+  FiShoppingCart,
   FiTv,
 } from 'react-icons/fi';
+import { useAuthContext } from '../contexts/AuthContext';
 
 const LinkItems = [
   { name: 'Home', icon: <FiHome />, url: '/' },
@@ -17,8 +19,13 @@ const LinkItems = [
 ];
 
 const Sidebar = () => {
+  const { username } = useAuthContext();
   return (
     <div className="fixed hidden h-full w-56 border-r-1 p-6 md:block">
+      <div className="my-4 flex items-center gap-2">
+        <FiShoppingCart size={24} />
+        <p className="font-bold uppercase">Receipts</p>
+      </div>
       <Listbox variant="faded" aria-label="Sidebar" label="hi" title="hi">
         {LinkItems.map((link) => (
           <ListboxItem key={link.name} href={link.url} textValue="hi">
@@ -28,7 +35,10 @@ const Sidebar = () => {
           </ListboxItem>
         ))}
       </Listbox>
-      <Button fullWidth startContent={<FiLogOut />}>Sign out</Button>
+      <h4 className="my-2 text-center">{username}</h4>
+      <Button fullWidth startContent={<FiLogOut />}>
+        Sign out
+      </Button>
     </div>
   );
 };
