4876697bbbf10562fb5c8d19ddc0b5032a1d383d
Dogukan Arslan <dogukan2arslan@hotmail.com>
2024-11-21 02:07:38 +0300

    Add sign up link

--- src/SignIn.jsx
+++ src/SignIn.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { Link } from 'react-router-dom';
 import { Button, Input } from '@nextui-org/react';
 import { useAuthContext } from './contexts/AuthContext';
 
@@ -15,22 +16,27 @@ export const SignIn = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit} className="space-y-2">
-      <h1>Sign In</h1>
-      <Input
-        label="Username"
-        value={username}
-        required
-        onChange={(e) => setUsername(e.target.value)}
-      />
-      <Input
-        type="password"
-        label="Password"
-        value={password}
-        required
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <Button type="submit">Sign in</Button>
-    </form>
+    <>
+      <form onSubmit={handleSubmit} className="space-y-2">
+        <h1>Sign In</h1>
+        <Input
+          label="Username"
+          value={username}
+          required
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <Input
+          type="password"
+          label="Password"
+          value={password}
+          required
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <Button type="submit">Sign in</Button>
+      </form>
+      <div className="mt-2">
+        <Link to="/signup">Create an account</Link>
+      </div>
+    </>
   );
 };
