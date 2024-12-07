481b4f33086fca47680f6d00340a7fbf2a36ab22
Dogukan Arslan <dogukan2arslan@hotmail.com>
2024-12-07 20:51:34 +0300

    Register user

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import { createClient } from '@libsql/client';
 import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const path = __dirname + '/.env';
@@ -36,6 +37,32 @@ app.post('/api/register', async (req, res) => {
   } catch (e) {
     console.log(e);
     res.status(500).send('Internal server error');
+  }
+});
+
+app.post('/api/login', async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const users = await db.execute({
+      sql: 'SELECT * FROM users WHERE username = ?',
+      args: [username],
+    });
+
+    const user = users.rows[0];
+    if (!user) {
+      return res.status(401).json({ error: 'Invalid credentials' });
+    }
+
+    const passwordMatch = await bcrypt.compare(password, user.password);
+    if (!passwordMatch) {
+      return res.status(401).json({ error: 'Invalid credentials' });
+    }
+
+    const token = jwt.sign({ username: user.username }, process.env.JWT_SECRET);
+    res.status(200).json({ token });
+  } catch (e) {
+    console.log(e)
+    res.status(500).json({ error: 'Internal server error' });
   }
 });
 
